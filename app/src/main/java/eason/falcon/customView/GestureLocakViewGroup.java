983a533492601e15ab27e9a0d1911ded0792d38c
FalsonG <848311545@qq.com>
2016-07-05 18:14:43 +0800

    over

--- app/src/main/java/eason/falcon/customView/GestureLocakViewGroup.java
+++ app/src/main/java/eason/falcon/customView/GestureLocakViewGroup.java
@@ -5,10 +5,12 @@ import android.content.res.TypedArray;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,25 +24,25 @@ public class GestureLocakViewGroup extends RelativeLayout {
 
     private GestureLockView[] mGestureLockViews;
 
-    private int mCount=3;
+    private int mCount = 3;
 
-    private ArrayList<Integer> mAnswer=null;
+    private ArrayList<Integer> mAnswer = new ArrayList<Integer>();
 
-    private List<Integer> mChoose=new ArrayList<Integer>();
+    private List<Integer> mChoose = new ArrayList<Integer>();
 
     private Paint mPaint;
 
-    private int mMarginBetweenLockView=30;
+    private int mMarginBetweenLockView = 30;
 
     private int mGestureLockViewWidth;
 
-    private int mNoFingerInnerCircleColor=0xFF939090;
+    private int mNoFingerInnerCircleColor = 0xFF939090;
 
-    private int mNoFingerOuterCircleColr=0xFFE0DBDB;
+    private int mNoFingerOuterCircleColr = 0xFFE0DBDB;
 
-    private int mFingerOnColor=0xFF378FC9;
+    private int mFingerOnColor = 0xFF378FC9;
 
-    private int mFingerUpColor=0xFFFF0000;
+    private int mFingerUpColor = 0xFFFF0000;
 
     private int mWidth;
 
@@ -52,50 +54,45 @@ public class GestureLocakViewGroup extends RelativeLayout {
 
     private int mLastPathY;
 
-    private Point mTmpTarget=new Point();//结束位置
+    private Point mTmpTarget = new Point();//结束位置
 
-    private int mTryTimes=4;//最大尝试次数
+    private int mTryTimes = 4;//最大尝试次数
 
     private OnGestureLockViewListener mOnGestureLockViewListener;
 
-    public GestureLocakViewGroup(Context context,AttributeSet attributeSet)
-    {
-        super(context,attributeSet);
-        initResources(context,attributeSet);
+    public GestureLocakViewGroup(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        initResources(context, attributeSet);
     }
 
-    public GestureLocakViewGroup(Context context,AttributeSet attrs,int defStyle)
-    {
-        super(context,attrs,defStyle);
-        initResources(context,attrs);
+    public GestureLocakViewGroup(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initResources(context, attrs);
     }
 
-    private void initResources(Context context,AttributeSet attrs)
-    {
-        TypedArray a= context.getTheme().obtainStyledAttributes(attrs, R.styleable.GestureLockViewGroup,0,0);
-        int n=a.getIndexCount();
-        for(int i=0;i<n;++i)
-        {
-            int attr=a.getIndex(i);
-            switch(attr)
-            {
+    private void initResources(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.GestureLockViewGroup, 0, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; ++i) {
+            int attr = a.getIndex(i);
+            switch (attr) {
                 case R.styleable.GestureLockViewGroup_color_no_finger_inner_circle:
-                    mNoFingerInnerCircleColor=a.getColor(attr,mNoFingerInnerCircleColor);
+                    mNoFingerInnerCircleColor = a.getColor(attr, mNoFingerInnerCircleColor);
                     break;
                 case R.styleable.GestureLockViewGroup_color_no_finger_outer_circle:
-                    mNoFingerOuterCircleColr=a.getColor(attr,mNoFingerOuterCircleColr);
+                    mNoFingerOuterCircleColr = a.getColor(attr, mNoFingerOuterCircleColr);
                     break;
                 case R.styleable.GestureLockViewGroup_color_finger_on:
-                    mFingerOnColor=a.getColor(attr,mFingerOnColor);
+                    mFingerOnColor = a.getColor(attr, mFingerOnColor);
                     break;
                 case R.styleable.GestureLockViewGroup_color_finger_up:
-                    mFingerUpColor=a.getColor(attr,mFingerUpColor);
+                    mFingerUpColor = a.getColor(attr, mFingerUpColor);
                     break;
                 case R.styleable.GestureLockViewGroup_count:
-                    mCount=a.getInt(attr,3);
+                    mCount = a.getInt(attr, 3);
                     break;
                 case R.styleable.GestureLockViewGroup_tryTimes:
-                    mTryTimes=a.getInt(attr,5);
+                    mTryTimes = a.getInt(attr, 5);
                     break;
                 default:
                     break;
@@ -104,146 +101,209 @@ public class GestureLocakViewGroup extends RelativeLayout {
         }
         a.recycle();
 
-        mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPath=new Path();
+        mPath = new Path();
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec,int heightMeasureSpec)
-    {
-        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        mWidth=MeasureSpec.getSize(widthMeasureSpec);
-        mHeight=MeasureSpec.getSize(heightMeasureSpec);
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
 
-        mHeight=mWidth=mWidth<mHeight?mWidth:mHeight;
+        mHeight = mWidth = mWidth < mHeight ? mWidth : mHeight;
 
-        if(mGestureLockViews==null)
-        {
-            mGestureLockViews=new GestureLockView[mCount*mCount];
-            mGestureLockViewWidth=(int)(4*mWidth*1.0f/(5*mCount+1));
-            mMarginBetweenLockView=(int)(mGestureLockViewWidth*0.25);
-            mPaint.setStrokeWidth(mGestureLockViewWidth*029f);
-            for(int i=0;i<mGestureLockViews.length;++i)
-            {
-                mGestureLockViews[i]=new GestureLockView(getContext(),mNoFingerInnerCircleColor,mNoFingerOuterCircleColr,mFingerOnColor,mFingerUpColor);
-                mGestureLockViews[i].setId(i+1);
-                RelativeLayout.LayoutParams lockerParasm=new RelativeLayout.LayoutParams(mGestureLockViewWidth,mGestureLockViewWidth);
+        if (mGestureLockViews == null) {
+            mGestureLockViews = new GestureLockView[mCount * mCount];
+            mGestureLockViewWidth = (int) (4 * mWidth * 1.0f / (5 * mCount + 1));
+            mMarginBetweenLockView = (int) (mGestureLockViewWidth * 0.25);
+            mPaint.setStrokeWidth(mGestureLockViewWidth * 029f);
+            for (int i = 0; i < mGestureLockViews.length; ++i) {
+                mGestureLockViews[i] = new GestureLockView(getContext(), mNoFingerInnerCircleColor, mNoFingerOuterCircleColr, mFingerOnColor, mFingerUpColor);
+                mGestureLockViews[i].setId(i + 1);
+                RelativeLayout.LayoutParams lockerParasm = new RelativeLayout.LayoutParams(mGestureLockViewWidth, mGestureLockViewWidth);
 
-                if(i%mCount!=0)
-                {
-                    lockerParasm.addRule(RelativeLayout.RIGHT_OF,mGestureLockViews[i-1].getId());
+                if (i % mCount != 0) {
+                    lockerParasm.addRule(RelativeLayout.RIGHT_OF, mGestureLockViews[i - 1].getId());
                 }
-                if(i>mCount-1)
-                {
-                    lockerParasm.addRule(RelativeLayout.BELOW,mGestureLockViews[i-mCount].getId());
+                if (i > mCount - 1) {
+                    lockerParasm.addRule(RelativeLayout.BELOW, mGestureLockViews[i - mCount].getId());
                 }
 
-                int rightMargin=mMarginBetweenLockView;
-                int bottomMargin=mMarginBetweenLockView;
-                int leftMargin=0;
-                int topMargin=0;
+                int rightMargin = mMarginBetweenLockView;
+                int bottomMargin = mMarginBetweenLockView;
+                int leftMargin = 0;
+                int topMargin = 0;
 
-                if(i>=0&&i<mCount)
-                {
-                    topMargin=mMarginBetweenLockView;
+                if (i >= 0 && i < mCount) {
+                    topMargin = mMarginBetweenLockView;
                 }
-                if(i%mCount==0)
-                {
-                    leftMargin=mMarginBetweenLockView;
+                if (i % mCount == 0) {
+                    leftMargin = mMarginBetweenLockView;
                 }
-                lockerParasm.setMargins(leftMargin,topMargin,rightMargin,bottomMargin);
+                lockerParasm.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
                 mGestureLockViews[i].setMode(GestureLockView.Mode.STATUS_NO_FINGER);
-                addView(mGestureLockViews[i],lockerParasm);
+                addView(mGestureLockViews[i], lockerParasm);
             }
         }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        int action=event.getAction();
-        int x=(int)event.getX();
-        int y=(int)event.getY();
-        switch (action)
-        {
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (action) {
             case MotionEvent.ACTION_DOWN:
+                reset();
                 break;
             case MotionEvent.ACTION_MOVE:
+                mPaint.setColor(mFingerOnColor);
+                mPaint.setAlpha(50);
+                GestureLockView child = getChildByPos(x, y);
+                if (null != child) {
+                    int cId = child.getId();
+                    if (!mChoose.contains(cId)) {
+                        mChoose.add(cId);
+                        child.setMode(GestureLockView.Mode.STATUS_FINGER_ON);
+                        if (mOnGestureLockViewListener != null) {
+                            mOnGestureLockViewListener.onBlockSelected(cId);
+                            mLastPathX = child.getLeft() / 2 + child.getRight() / 2;
+                            mLastPathY = child.getTop() / 2 + child.getBottom() / 2;
+                            if (mChoose.size() == 1) {
+                                mPath.moveTo(mLastPathX, mLastPathY);
+                            } else {
+                                mPath.lineTo(mLastPathX, mLastPathY);
+                            }
+                        }
+                        mTmpTarget.x = x;
+                        mTmpTarget.y = y;
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mPaint.setColor(mFingerUpColor);
+                mPaint.setAlpha(50);
+                --mTryTimes;
+
+                boolean result = checkAnswers();
+
+                if (null != mOnGestureLockViewListener && mChoose.size() > 0) {
+                    mOnGestureLockViewListener.onGestureEvent(result);
+                    if (mTryTimes == 0) {
+                        mOnGestureLockViewListener.onUnmatchedExceedBoundary();
+                    }
+                }
+                mTmpTarget.x = mLastPathX;
+                mTmpTarget.y = mLastPathY;
+                changeItemMode();
+                for (int i = 0; i + 1 < mChoose.size(); i++) {
+                    int childId = mChoose.get(i);
+                    int nextChildId = mChoose.get(i + 1);
+                    GestureLockView startChild = (GestureLockView) findViewById(childId);
+                    GestureLockView nextChild = (GestureLockView) findViewById(nextChildId);
+
+                    int dx = nextChild.getLeft() - startChild.getLeft();
+                    int dy = nextChild.getTop() - startChild.getTop();
+
+                    int angle = (int) Math.toDegrees(Math.atan2(dy, dx)) + 90;
+                    startChild.setArrowDegree(angle);
+                }
+                if (!mAnswer.isEmpty()) {
+                    mAnswer.clear();
+                    mAnswer.addAll(mChoose);
+
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getContext(), "手势设置成功!", Toast.LENGTH_SHORT).show();
+                            reset();
+                        }
+                    }, 2000);
+                } else {
+                    mAnswer.clear();
+                }
+                invalidate();
                 break;
 
         }
         return super.onTouchEvent(event);
     }
 
-    private void reset()
+    public void setAnswer(List<Integer> answers)
     {
+        this.mAnswer.addAll(answers);
+    }
+
+    public void setUnMatchExceedBoundary(int boundary)
+    {
+        this.mTryTimes=boundary;
+    }
+
+    private void changeItemMode() {
+        for (GestureLockView gestureLockView : mGestureLockViews) {
+            if (mChoose.contains(gestureLockView.getId())) {
+                gestureLockView.setMode(GestureLockView.Mode.STATUS_FINGER_UP);
+            }
+        }
+    }
+
+    private void reset() {
         mChoose.clear();
         mPath.reset();
-        for(GestureLockView cestureLockView :mGestureLockViews)
-        {
+        for (GestureLockView cestureLockView : mGestureLockViews) {
             cestureLockView.setMode(GestureLockView.Mode.STATUS_NO_FINGER);
             cestureLockView.setArrowDegree(-1);
         }
     }
 
-    private boolean checkAnswers()
-    {
-        if(mAnswer==null)
-        {
+    private boolean checkAnswers() {
+        if (mAnswer == null) {
             return true;
         }
 
-        if(mAnswer.size()!=mChoose.size())
-        {
+        if (mAnswer.size() != mChoose.size()) {
             return false;
         }
 
-        for(int i=0,len=mAnswer.size();i<len;++i)
-        {
-            if(mAnswer.get(i)!=mChoose.get(i))
-            {
-                return  false;
+        for (int i = 0, len = mAnswer.size(); i < len; ++i) {
+            if (mAnswer.get(i) != mChoose.get(i)) {
+                return false;
             }
         }
 
         return true;
     }
 
-    private boolean checkPositionInChild(View child, int x, int y)
-    {
-        int  padding=(int)(mGestureLockViewWidth*0.15);
-        if(x>child.getLeft()+padding&&y>child.getTop()+padding&&x<child.getRight()-padding&&y<child.getBottom()-padding)
-        {
+    private boolean checkPositionInChild(View child, int x, int y) {
+        int padding = (int) (mGestureLockViewWidth * 0.15);
+        if (x > child.getLeft() + padding && y > child.getTop() + padding && x < child.getRight() - padding && y < child.getBottom() - padding) {
             return true;
         }
         return false;
     }
 
-    private GestureLockView getChildByPos(int x,int y)
-    {
-        for(GestureLockView gestureLockView:mGestureLockViews)
-        {
-            if(checkPositionInChild(gestureLockView,x,y))
-            {
+    private GestureLockView getChildByPos(int x, int y) {
+        for (GestureLockView gestureLockView : mGestureLockViews) {
+            if (checkPositionInChild(gestureLockView, x, y)) {
                 return gestureLockView;
             }
         }
         return null;
     }
 
-    public void setGestureLockViewListener(OnGestureLockViewListener listener)
-    {
-        this.mOnGestureLockViewListener=listener;
+    public void setGestureLockViewListener(OnGestureLockViewListener listener) {
+        this.mOnGestureLockViewListener = listener;
     }
 
 
-
-    public interface OnGestureLockViewListener{
+    public interface OnGestureLockViewListener {
         public void onBlockSelected(int cId);
+
         public void onGestureEvent(boolean matched);
+
         public void onUnmatchedExceedBoundary();
     }
 }
--- app/src/main/java/eason/falcon/studydemo/MainActivity.java
+++ app/src/main/java/eason/falcon/studydemo/MainActivity.java
@@ -2,12 +2,47 @@ package eason.falcon.studydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import eason.falcon.customView.GestureLocakViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureLocakViewGroup mGestureLockViewGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGestureLockViewGroup = (GestureLocakViewGroup) findViewById(R.id.id_gestureLockViewGroup);
+        ArrayList<Integer> answers = new ArrayList<>();
+        answers.add(1);
+        answers.add(2);
+        answers.add(3);
+        answers.add(4);
+        answers.add(5);
+        mGestureLockViewGroup.setAnswer(answers);
+        mGestureLockViewGroup
+                .setGestureLockViewListener(new GestureLocakViewGroup.OnGestureLockViewListener() {
+
+                    @Override
+                    public void onUnmatchedExceedBoundary() {
+                        Toast.makeText(MainActivity.this, "错误5次...",
+                                Toast.LENGTH_SHORT).show();
+                        mGestureLockViewGroup.setUnMatchExceedBoundary(5);
+                    }
+
+                    @Override
+                    public void onGestureEvent(boolean matched) {
+                        Toast.makeText(MainActivity.this, matched + "",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onBlockSelected(int cId) {
+                    }
+                });
     }
 }
